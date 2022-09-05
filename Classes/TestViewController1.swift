7f41dd57425f1bfc57330de61eec07558cd501ce
kongwen <kongwen@xiaohongshu.com>
2022-09-05 19:54:08 +0800

    增加文件

---
+++ Classes/TestViewController1.swift
@@ -0,0 +1,23 @@
+//
+//  TestViewController1.swift
+//  TestFramework1
+//
+//  Created by kongwen on 2022/9/5.
+//
+
+import Foundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
